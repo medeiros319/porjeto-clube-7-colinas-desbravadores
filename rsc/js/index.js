446f796bcb2101c882503bc469d44b77a8a6bbb8
João Victor <jaovictormedeiros9@gmail.com>
2023-04-15 15:56:55 -0300

    Carrossel atualizado

--- rsc/js/index.js
+++ rsc/js/index.js
@@ -1,1 +1,33 @@
-//uyfhchdtfcgfcgrdtd
+// pegar o elemento HTML dos botões 
+
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+const textos = document.querySelectorAll('.descricao')
+
+// indentificar o clique do botão
+
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        // desmacar o botão anterior
+        const botaoSelecionado = document.querySelector('.selecionado');
+        botaoSelecionado.classList.remove('selecionado');
+        
+        // marcar o botão selecionado 
+        botao.classList.add('selecionado')        
+        
+        // esconder a imagem anterior 
+        const imagemAtiva = document.querySelector('.ativa');
+        imagemAtiva.classList.remove('ativa');
+
+        // fazer aparecer a imagem correspondete ao botão clicado  
+        imagens[indice].classList.add('ativa');
+
+        //--------------------------------------Firula minha :3---------------------------------------------//
+
+        // const textoAtivo = document.querySelector('.tela');
+        // textoAtivo.classList.remove('tela');
+
+        // textos[indice].classList.add('tela')
+
+    })
+})
